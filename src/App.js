d713d42b990957caddc31222f85cf39544cf560e
Asmaa <asmaakhdr24@gmail.com>
2023-01-05 22:42:37 +0200

    http states with redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
 import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+import { uiActions } from './store/ui-slice';
+
+let isInitial = true;
 
 function App() {
   const cartIsVisible = useSelector((state) => state.ui.cartIsVisible);
+  const dispatch = useDispatch();
 
   const cart = useSelector((state) => state.cart);
 
+  const notifications = useSelector((state) => state.ui.notifications);
+
   useEffect(() => {
-    fetch('https://react-http-ffae2-default-rtdb.firebaseio.com/cart.json', {
-      method: 'PUT',
-      body: JSON.stringify(cart),
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'Pending',
+          title: 'Sending...',
+          message: 'Sending cart data',
+        })
+      );
+      const response = await fetch(
+        'https://react-http-ffae2-default-rtdb.firebaseio.com/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+        }
+      );
+      if (!response.ok) {
+        throw new Error('Error');
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sent cart data successfully!',
+        })
+      );
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: error,
+        })
+      );
     });
-  }, [cart]);
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {cartIsVisible && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notifications && (
+        <Notification
+          status={notifications.status}
+          title={notifications.title}
+          message={notifications.message}
+        />
+      )}
+      <Layout>
+        {cartIsVisible && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
