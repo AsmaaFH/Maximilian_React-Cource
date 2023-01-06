988803f3a2b422d074e66863b504ad735ced4ea4
Asmaa <asmaakhdr24@gmail.com>
2023-01-06 22:44:38 +0200

    nested routes

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Link, Route } from 'react-router-dom';
 
 const Welcome = () => {
-  return <h1>Welcome</h1>;
+  return (
+    <>
+      <h1>Welcome</h1>
+      <Link to={'/welcome/user'}>user</Link>
+
+      <Route path={'/welcome/user'}>
+        <p>User Inside Welcome</p>
+      </Route>
+    </>
+  );
 };
 
 export default Welcome;
