0471fb0a150b385bb367469da345b1c380d5ce00
Asmaa <asmaakhdr24@gmail.com>
2023-01-07 01:08:40 +0200

    Prompt Cmp to prevent un want transition

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -10,6 +10,9 @@ const MainNavigation = () => {
         <nav className={classes.nav}>
           <ul>
             <li>
+              <NavLink to={'quotes'} activeClassName={classes.active}>
+                All Quotes
+              </NavLink>
               <NavLink to={'new-quote'} activeClassName={classes.active}>
                 Add a Quote
               </NavLink>
--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -6,7 +6,6 @@ const NewQuote = () => {
   const history = useHistory();
 
   const addQuoteHandler = (quoteData) => {
-    console.log(quoteData);
     history.push('/quotes');
   };
   return <QuoteForm onAddQuote={addQuoteHandler} />;
