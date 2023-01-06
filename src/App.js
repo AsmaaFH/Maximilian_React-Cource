af0882ba7df61e2a92219e2b8702aa0fe990b27c
Asmaa <asmaakhdr24@gmail.com>
2023-01-07 00:35:11 +0200

    not found page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import AllQoutes from './pages/AllQoutes';
 import NewQuote from './pages/NewQuote';
+import NotFound from './pages/NotFound';
 import QuoteDetail from './pages/QuoteDetail';
 
 function App() {
   return (
     <div>
       <Layout>
-        <Route path={'/'} exact>
-          <Redirect to={'/quotes'} />
-        </Route>
-        <Route path={'/quotes'} exact>
-          <AllQoutes />
-        </Route>
-        <Route path={'/quotes/:id'}>
-          <QuoteDetail />
-        </Route>
-        <Route path={'/new-quote'}>
-          <NewQuote />
-        </Route>
+        <Switch>
+          <Route path={'/'} exact>
+            <Redirect to={'/quotes'} />
+          </Route>
+          <Route path={'/quotes'} exact>
+            <AllQoutes />
+          </Route>
+          <Route path={'/quotes/:id'}>
+            <QuoteDetail />
+          </Route>
+          <Route path={'/new-quote'}>
+            <NewQuote />
+          </Route>
+          <Route path={'*'}>
+            <NotFound />
+          </Route>
+        </Switch>
       </Layout>
     </div>
   );
