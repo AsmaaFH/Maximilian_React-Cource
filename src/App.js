9f17baab996be5460bccbd84f2180178822a2a72
Asmaa <asmaakhdr24@gmail.com>
2023-01-06 22:39:24 +0200

    router exact

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route } from 'react-router-dom';
 import Header from './components/Header';
+import Home from './pages/Home';
 import Products from './pages/Products';
 import ProductsDeatails from './pages/ProductsDeatails';
 import Welcome from './pages/Welcome';
@@ -8,13 +9,17 @@ function App() {
   return (
     <div>
       <Header />
+
+      <Route path="/" exact>
+        <Home />
+      </Route>
       <Route path="/welcome">
         <Welcome />
       </Route>
-      <Route path="/products">
+      <Route path="/products" exact>
         <Products />
       </Route>
-      <Route path="/product-detail/:productId">
+      <Route path="/products/:productId">
         <ProductsDeatails />
       </Route>
     </div>
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Products = () => {
   return (
     <ul>
-      <li>Book</li>
-      <li>Product</li>
-      <li>Carbpet</li>
+      <li>
+        <Link to="/products/p1">Book</Link>
+      </li>
+      <li>
+        <Link to="/products/p2">Product</Link>
+      </li>
+      <li>
+        <Link to="/products/p3">Carbpet</Link>
+      </li>
     </ul>
   );
 };
