b7d2652cd38e0fc2c13fb962935b1bb984290efc
Asmaa <asmaakhdr24@gmail.com>
2023-01-07 00:30:33 +0200

    ...

--- src/pages/AllQoutes.js
+++ src/pages/AllQoutes.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import QuoteList from '../components/quotes/QuoteList';
 
 const AllQoutes = () => {
-  return <div>AllQoutes</div>;
+  return (
+    <QuoteList
+      quotes={[
+        { id: '1', author: 'author1', text: 'Twxt1' },
+        { id: '2', author: 'author2', text: 'Text2' },
+      ]}
+    />
+  );
 };
 
 export default AllQoutes;
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Link, Route, useParams } from 'react-router-dom';
 import Comments from '../components/comments/Comments';
+import QuoteItem from '../components/quotes/QuoteItem';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
 function QuoteDetail() {
   const param = useParams();
 
+  const DUMMY = [
+    { id: '1', author: 'author1', text: 'Twxt1' },
+    { id: '2', author: 'author2', text: 'Text2' },
+  ];
+
+  let quote = DUMMY.find((quote) => quote.id == param.id);
+
+  if (!quote) {
+    return <p>No Quotes Found!</p>;
+  }
+
   return (
     <>
-      <h1>Details</h1>
+      <HighlightedQuote author={quote.author} text={quote.text} />
       <h1>{param.id}</h1>
-      <Link to={`/quotes/${param.id}/comments`}>Comments</Link>
       <Route path={'/quotes/:id/comments'}>
         <Comments />
       </Route>
