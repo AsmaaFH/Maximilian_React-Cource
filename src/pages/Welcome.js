7593be6f3f2107ffd745e563c2601d287b9c9042
Asmaa <asmaakhdr24@gmail.com>
2023-01-07 20:47:23 +0200

    from router v5 to v6

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,5 +1,14 @@
+import { Link, Outlet } from 'react-router-dom';
+
 const Welcome = () => {
-  return <h1>The Welcome Page</h1>;
+  return (
+    <>
+      <h1>The Welcome Page</h1>
+      <Link to={'new-user'}>Go to new user</Link>
+
+      <Outlet />
+    </>
+  );
 };
 
 export default Welcome;
