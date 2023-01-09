7f1d7180e0b131efdbc07ab566e5f6f7b23c2167
Asmaa <asmaakhdr24@gmail.com>
2023-01-09 10:40:51 +0200

    action (new post)

--- src/components/NewPostForm.jsx
+++ src/components/NewPostForm.jsx
@@ -1,28 +1,20 @@
 import classes from './NewPostForm.module.css';
 
-function NewPostForm({ onCancel, onSubmit, submitting }) {
+function NewPostForm({ onCancel, submitting }) {
   return (
-    <form className={classes.form} onSubmit={onSubmit}>
+    <form className={classes.form} action="/blog/new" method="post">
       <fieldset>
         <label htmlFor="title">Title</label>
         <input id="title" type="text" name="title" required minLength={5} />
       </fieldset>
       <fieldset>
         <label htmlFor="text">Post Text</label>
-        <textarea
-          id="text"
-          name="post-text"
-          required
-          minLength={10}
-          rows={5}
-        ></textarea>
+        <textarea id="text" name="post-text" required minLength={10} rows={5}></textarea>
       </fieldset>
       <button type="button" onClick={onCancel} disabled={submitting}>
         Cancel
       </button>
-      <button disabled={submitting}>
-        {submitting ? 'Submitting...' : 'Create Post'}
-      </button>
+      <button disabled={submitting}>{submitting ? 'Submitting...' : 'Create Post'}</button>
     </form>
   );
 }
