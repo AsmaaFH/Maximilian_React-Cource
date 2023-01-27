a3cf0e3609c2db8e4e2f2528a170f679b52aaa80
Asmaa <asmaakhdr24@gmail.com>
2023-01-27 09:45:42 +0200

    using typescript

---
+++ src/components/Todos.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Todo from "../models/todos"
+import TodoItem from "./TodoItem"
+
+
+const Todos: React.FC<{items: Todo[]}> = (props) => {
+  return (
+    <ul>
+      {props.items.map((item,index)=> <TodoItem key={index} text={item.text}/>)} 
+    </ul>
+  )
+}
+
+export default Todos
